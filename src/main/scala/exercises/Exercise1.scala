5e61d6ebcb2dc16d542c2fdcad49d9166e773e6a
CruzLeyvaSegundo <sdjcl95@gmail.com>
2025-09-27 22:25:48 -0500

    Add solution for exercise 1

--- src/main/scala/exercises/Exercise1.scala
+++ src/main/scala/exercises/Exercise1.scala
@@ -2,11 +2,36 @@ package exercises
 
 object Exercise1 {
   case class Rate(rateCode: String, rateGroup: String)
-
   case class CabinPrice(cabinCode: String, rateCode: String, price: BigDecimal)
-
   case class BestGroupPrice(cabinCode: String, rateCode: String, price: BigDecimal, rateGroup: String)
 
-  def getBestGroupPrices(rates: Seq[Rate], prices: Seq[CabinPrice]): Seq[BestGroupPrice] = ???
+  def getBestGroupPrices(rates: Seq[Rate], prices: Seq[CabinPrice]): Seq[BestGroupPrice] = {
+    val rateCodesByRateGroup = rates.groupMap(_.rateGroup)(_.rateCode).toSeq
+
+    val bestGroupPrices = rateCodesByRateGroup
+      .flatMap { case (rateGroup, rateCodes) =>
+        val cabinPricesCandidates = prices
+          .filter(cabinPrice => rateCodes.contains(cabinPrice.rateCode))
+
+        val bestGroupPricesByRateGroupAndCabinCode = cabinPricesCandidates
+          .groupBy(_.cabinCode)
+          .map { case (_, cabinPrices) =>
+            val bestCabinPriceCandidateByCabinCode = cabinPrices.minBy(_.price)
+
+            BestGroupPrice(
+              bestCabinPriceCandidateByCabinCode.cabinCode,
+              bestCabinPriceCandidateByCabinCode.rateCode,
+              bestCabinPriceCandidateByCabinCode.price,
+              rateGroup
+            )
+          }
+          .toSeq
+
+        bestGroupPricesByRateGroupAndCabinCode
+      }
+
+    bestGroupPrices
+      .sortBy(groupPrice =>(groupPrice.cabinCode, groupPrice.rateCode))
+  }
 
 }
